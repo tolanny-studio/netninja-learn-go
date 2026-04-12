a6a31dd4064b0599d10dd97f7d326b6952b2833b
tolanny-studio <hellotolannystudio@gmail.com>
2026-04-12 23:15:54 +0100

    commit exercise-2 directory. A variable directory

---
+++ exercise-2/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// variable declaration
+
+	// strings
+	var nameOne string = "taoheed"
+	// variable type inferred
+	var nameTwo = "tolani"
+	var nameThree string
+	nameThree = "Akanji"
+	extraName := "Adeleke"
+
+	fmt.Println(nameOne, nameTwo, nameThree, extraName)
+
+	// ints
+
+	ageOne := 45
+	var ageTwo int = 56
+	var ageThree = 90
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits and memory
+
+	var numOne int8 = 32
+	var numTwo uint = 25
+
+	// float 
+
+	var scoreOne float32 = 34.90
+	var scoreTwo float64
+
+}
